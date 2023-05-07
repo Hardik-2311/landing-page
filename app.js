23aec44c200365aef2cbab8ec10b769629d8a8bd
Hardik-2311 <126074137+Hardik-2311@users.noreply.github.com>
2023-05-07 18:47:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var navlinks=document.getElementById("navlinks");
+function showmenu(){
+    navlinks.style.right="0";
+}
+function hidemenu(){
+    navlinks.style.right="-200px";
+}
+
+
+// const content=document.getElementsByClassName("content-box");
+// for(i=0;i<content.length;i++){
+//     content[i].addEventListener('click',function(){
+//         this.classList.toggle('active')
+//     })
+// }
+
+
+
